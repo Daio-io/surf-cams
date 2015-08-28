ae229525cd40a1ad42dbd7a7f576fec10aedc989
Dai <dai.williams@bbc.co.uk>
2015-08-28 14:41:17 +0100

    Handle db connection string with env variable

--- app/db/connect.go
+++ app/db/connect.go
@@ -2,15 +2,26 @@ package db
 
 import (
 	"gopkg.in/mgo.v2"
+	"os"
+	"strings"
 )
 
-var connectionString = "mongodb://localhost/"
+var connectionString = getConnectionString()
 
 // Connect to database - returns a session
 func Connect() *mgo.Session {
+
 	session, err := mgo.Dial(connectionString)
 	if err != nil {
 		panic(err)
 	}
 	return session
+}
+
+func getConnectionString() string {
+	connectionString := os.Getenv("DB_CONNECTION")
+	if len(strings.TrimSpace(connectionString)) == 0 {
+		connectionString = "mongodb://localhost/"
+	}
+	return connectionString
 }
