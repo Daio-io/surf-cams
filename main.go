ded08b99757d72d0ef4e4bfc681df3af697ccbe8
Dai <dai.williams@bbc.co.uk>
2015-08-29 12:02:26 +0100

    Added status page

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ var port = getPort()
 
 func main() {
 	router := gin.Default()
-	router.GET("/:id", surfcam.GetCamByID)
+	router.GET("/id/:id", surfcam.GetCamByID)
+	router.GET("/status", func(c *gin.Context) {
+		c.String(200, "OK")
+	})
 	router.Run(":" + port)
 }
 
